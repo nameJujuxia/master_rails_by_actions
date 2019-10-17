f58fa5d79cb691caff59ee59d8386bf5c0621faf
居治华 <juzhihua@juzhihuadeMacBook-Pro.local>
2019-10-17 14:26:51 +0800

    [add]-分类创建功能

--- Gemfile
+++ Gemfile
@@ -17,8 +17,6 @@ gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 gem 'therubyracer', platforms: :ruby
 
-gem 'ancestry'
-
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Use CoffeeScript for .coffee assets and views
@@ -36,6 +34,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'bootstrap-sass'
 gem 'font-awesome-rails'
 gem 'sorcery'
+gem 'ancestry'
+gem 'will_paginate'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,22 @@
 class Admin::CategoriesController < Admin::BaseController
   def index
+    @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: "desc")
   end
 
   def new
+    @category = Category.new
+    @root_categories = Category.roots.order(id: "desc")
+  end
+
+  def create
+    @category = Category.new(params.require(:category).permit!)
+    @root_categories = Category.roots.order(id: "desc")
+
+    if @category.save
+      flash[:notice] = "保存成功"
+      redirect_to admin_categories_path
+    else
+      render action: :new
+    end
   end
 end
