35bbef3be105c174b8be06193a4874f023bc9563
居治华 <juzhihua@juzhihuadeMacBook-Pro.local>
2019-10-17 15:39:03 +0800

    [add]-Product模型验证设置

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,23 @@
+class Admin::ProductsController < Admin::BaseController
+
+  def index
+    @product = Product.page(params[:page] || 1).per_page(params[:per_page] || 10)
+      .order("id desc")
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 class Product < ApplicationRecord
+
+  validates :amount, numericality: { only_integer: true, message: "库存必须为整数" },
+            if: proc { |product| !product.msrp.blank? }
+  validates :amount, presence: { message: "库存不能为空" }
+
+  validates :msrp, presence: { message: "MSRP不能为空" }
+  validates :msrp, numericality: { message: "MSRP必须为数字" },
+            if: proc { |product| !product.msrp.blank? }
+
+  validates :price, numericality: { message: "价格必须为数字" },
+            if: proc { |product| !product.msrp.blank? }
+  validates :price, presence: { message: "价格不能为空" }
+
+  validates :description, presence: { message: "描述不能为空" }
+
   belongs_to :category
   before_create :set_default_attrs
 
