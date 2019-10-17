3eb4b1caf7fe1e4e003b5efb746c9f105483841f
居治华 <juzhihua@juzhihuadeMacBook-Pro.local>
2019-10-17 09:45:28 +0800

    [add]-用户注册和登录功能

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+
+    def new
+    #   @is_using_email = true
+      @user = User.new
+    end
+  
+    def create
+    #   @is_using_email = (params[:user] and !params[:user][:email].nil?)
+  
+      @user = User.new(params.require(:user)
+        .permit(:email, :password, :password_confirmation))
+    #   @user.uuid = RandomCode.generate_utoken
+    #   update_browser_uuid @user.uuid
+  
+      if @user.save
+        flash[:notice] = "注册成功，请登录"
+        redirect_to new_session_path
+      else
+        render action: :new
+      end
+    end
+  
+  end
