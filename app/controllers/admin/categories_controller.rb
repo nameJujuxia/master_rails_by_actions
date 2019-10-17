f47e26116ffa4ca1901c594c6f17dd9856a3adc5
居治华 <juzhihua@juzhihuadeMacBook-Pro.local>
2019-10-17 14:53:51 +0800

    [add]-分类编辑和删除功能

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,16 +1,17 @@
 class Admin::CategoriesController < Admin::BaseController
+  before_action :find_root_categories, only: [:new, :create, :edit, :update]
+  before_action :find_category, only: [:edit, :update, :destroy]
+
   def index
     @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: "desc")
   end
 
   def new
     @category = Category.new
-    @root_categories = Category.roots.order(id: "desc")
   end
 
   def create
     @category = Category.new(params.require(:category).permit!)
-    @root_categories = Category.roots.order(id: "desc")
 
     if @category.save
       flash[:notice] = "保存成功"
@@ -18,5 +19,39 @@ class Admin::CategoriesController < Admin::BaseController
     else
       render action: :new
     end
+  end
+
+  def edit
+    render action: :new
+  end
+
+  def update
+    @category.attributes = params.require(:category).permit!
+
+    if @category.save
+      flash[:notice] = "修改成功"
+      redirect_to admin_categories_path
+    else
+      render action: :new
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      flash[:notice] = "删除成功"
+      redirect_to admin_categories_path
+    else
+      flash[:notice] = "删除失败"
+      redirect_to :back
+    end
+  end
+
+  private
+  def find_root_categories
+    @root_categories = Category.roots.order(id: "desc")
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
   end
 end
