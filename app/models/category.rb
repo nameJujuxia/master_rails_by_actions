f3b5f8130f0781702b0666137e763a5de8a924d5
居治华 <juzhihua@juzhihuadeMacBook-Pro.local>
2019-10-17 15:13:23 +0800

    [add]-二级分类管理功能

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,9 @@
 class Category < ApplicationRecord
 
   validates :title, presence: {message: "名称不能为空"}
+  validates :title, uniqueness: { message: "名称不能重复" }
 
-  has_ancestry
+  has_ancestry orphan_strategy: :destroy
 
   has_many :products, dependent: :destroy
 
