2fa50777840ad5c58db26d47d8e5021ddaa173ce
nameJujuxia <nameJujuxia@163.com>
2019-10-18 11:39:14 +0800

    [fix]-商品详情页

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,7 @@
 class CategoriesController < ApplicationController
 
   def show
-#     fetch_home_data
-    @categories = Category.grouped_data
+    fetch_home_data
 
     @category = Category.find(params[:id])
     @products = @category.products.onshelf.page(params[:page] || 1).per_page(params[:per_page] || 12)
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,8 @@ class ProductsController < ApplicationController
 
 
   def show
-    @categories = Category.grouped_data
+    fetch_home_data
+
     @product = Product.find(params[:id])
   end
 
