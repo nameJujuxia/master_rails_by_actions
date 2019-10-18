191b71f61b6b338e56e11f7cd0fb5043c628a044
nameJujuxia <nameJujuxia@163.com>
2019-10-18 22:43:41 +0800

    [add]-AddressesController和OrdersController开发以及用户登陆校验

---
+++ app/controllers/addresses_controller.rb
@@ -0,0 +1,10 @@
+class AddressesController < ApplicationController
+
+  layout false
+  before_action :auth_user
+
+  def index
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   resources :categories,only: [:show]
   resources :products,only: [:show]
   resources :shopping_carts
+  resources :addresses
+  resources :orders
 
   namespace :admin do
     root 'sessions#new'
